d8ae8dc04baf1259347ac11d8685142ca6c46d78
Steen Guo <84885004@qq.com>
2020-04-07 09:14:55 +0800

    Number类百分比过滤器增加%

--- src/number/index.js
+++ src/number/index.js
@@ -1,4 +1,9 @@
 const number = {
+  /**
+   * 千位分割
+   * @param {*} _value 被分割的数值
+   * @param {*} _symbol 分割符号
+   */
   thousand(_value, _symbol = " ") {
     const parts = `${_value || _value === 0 ? _value : ""}`.split(".");
     if (parts.length) {
@@ -14,17 +19,17 @@ const number = {
    */
   percent(_number, _decimals = 2) {
     let _temp = _number * 100;
-    return _temp.toFixed(_decimals);
+    return `${_temp.toFixed(_decimals)}%`;
   },
 
   /**
    * 四舍五入，保留x位小数
-   * @param {Number} _number 
+   * @param {Number} _number
    * @param {Number} _decimals 保留几位有效小数
    */
   toFixed(_number, _decimals = 2) {
-    return _number.toFixed(_decimals)
-  }
+    return _number.toFixed(_decimals);
+  },
 };
 
 export default number;
